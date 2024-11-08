3a90304f98c9b904e235ec0f9ff0e5859d27bf3d
deelaka99 <123wejith@gmail.com>
2024-11-08 17:50:11 +0530

    added change to app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Route, Routes, Navigate } from "react-router-dom";
+import Main from "./components/Main/index";
+import Signup from "./components/Signup/index";
+import Login from "./components/Login/index";
+
 function App() {
-  return <div className="App">Hello world!</div>;
+  const user = localStorage.getItem("token");
+  return (
+    <Routes>
+      {user && <Route path="/" exact element={<Main />} />}
+      <Route path="/signup" exact element={<Signup />} />
+      <Route path="/login" exact element={<Login />} />
+      <Route path="/" exact element={<Navigate replace to="/login" />} />
+    </Routes>
+  );
 }
 
 export default App;
