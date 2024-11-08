3acfba981c1991404ab5f841ae4144772e6b2616
deelaka99 <123wejith@gmail.com>
2024-11-08 17:42:53 +0530

    created main page

---
+++ src/components/Main/index.jsx
@@ -0,0 +1,19 @@
+import styles from "./styles.module.css";
+
+const Main = () => {
+  const handleLogout = () => {
+    localStorage.removeItem("tokn");
+    window.location.reload();
+  };
+
+  return (
+    <div className={styles.main_container}>
+      <nav className={styles.navbar}>
+        <h1>Fakebook</h1>
+        <button className={styles.white_btn} onClick={handleLogout}>
+          Logout
+        </button>
+      </nav>
+    </div>
+  );
+};
